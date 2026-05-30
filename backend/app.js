be093cc79ea2eb54337ab05ed947c34a725dd6e5
Shashank Shekhar <shashank091020@gmail.com>
2026-05-30 19:08:58 +0530

    chore: initialize frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Amaanitvam Platform API is running",
+  });
+});
+
+module.exports = app;
